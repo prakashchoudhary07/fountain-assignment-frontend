31e2ca9844d250bfd9594fc7ce6b673a8575acdb
Prakash <om.pcprakash@gmail.com>
2024-05-06 16:10:44 +0530

    feat: create useProfile hook

---
+++ src/hooks/useProfile.tsx
@@ -0,0 +1,17 @@
+"use client";
+import useSWR from "swr";
+import { API_SERVICE_URL } from "@/app/constants/url";
+import { fetcher } from "@/utils/swr";
+import { redirect } from "next/navigation";
+
+const PROFILE_API_URL = API_SERVICE_URL + "/api/v1/users/profile";
+
+export default function useProfile() {
+  const { data, error, isLoading } = useSWR(PROFILE_API_URL, fetcher);
+
+  return {
+    user: data,
+    isLoading,
+    isError: error,
+  };
+}
