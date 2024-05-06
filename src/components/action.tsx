a1df8f53122c8f342be05bc6c73cb867c9a7cfae
Prakash <om.pcprakash@gmail.com>
2024-05-06 10:52:18 +0530

    feature: create common action component to use in comments

---
+++ src/components/action.tsx
@@ -0,0 +1,11 @@
+import { FC, KeyboardEvent } from "react";
+
+const Action: FC<any> = ({ handelClick, name, className }) => {
+  return (
+    <button className={className} type="button" onClick={handelClick}>
+      {name}
+    </button>
+  );
+};
+
+export default Action;
